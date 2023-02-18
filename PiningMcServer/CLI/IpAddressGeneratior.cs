7d0ca47b3446cb939fc44c63c032daf4daff9663
Cootz <artem2004799@gmail.com>
2023-02-18 21:35:29 +0300

    Add commentaries for `IpAddressGeneratior` and `Options`

--- PiningMcServer/CLI/IpAddressGeneratior.cs
+++ PiningMcServer/CLI/IpAddressGeneratior.cs
@@ -9,8 +9,17 @@ using System.Threading.Tasks;
 
 namespace McServersScanner.CLI
 {
+    /// <summary>
+    /// Generates ranges of ip addresses depending on first and last addresses
+    /// </summary>
     public static class IpAddressGeneratior
     {
+        /// <summary>
+        /// Generates ranges of ip addresses depending on first and last addresses
+        /// </summary>
+        /// <param name="first">Start of range</param>
+        /// <param name="last">End of range</param>
+        /// <returns>Range of ip addresses</returns>
         public static IPAddress[] FillRange(IPAddress first, IPAddress last)
         { 
             List<IPAddress> result = new List<IPAddress>();
@@ -23,6 +32,12 @@ namespace McServersScanner.CLI
             return result.ToArray();
         }
 
+        /// <summary>
+        /// Generates ranges of ip addresses depending on first and last addresses
+        /// </summary>
+        /// <param name="first">Start of range</param>
+        /// <param name="last">End of range</param>
+        /// <returns>Range of ip addresses</returns>
         public static IPAddress[] FillRange(string first, string last) => FillRange(IPAddress.Parse(first), IPAddress.Parse(last));
 
         /// <summary>
@@ -35,6 +50,11 @@ namespace McServersScanner.CLI
             return BitConverter.ToUInt32(address.GetAddressBytes().Reverse().ToArray());
         }
 
+        /// <summary>
+        /// Converts int to IpAddress
+        /// </summary>
+        /// <param name="address"></param>
+        /// <returns></returns>
         private static IPAddress ConvertIntToIpAddress(uint address)
         {
             return new IPAddress(BitConverter.GetBytes(address).Reverse().ToArray());
--- PiningMcServer/CLI/Options.cs
+++ PiningMcServer/CLI/Options.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 
 namespace McServersScanner.CLI
 {
+    /// <summary>
+    /// Stores all possible options for command line
+    /// </summary>
     public class Options
     {
         [Option('I', "range", Required = true, HelpText = "Ip adress range to be processed.")]

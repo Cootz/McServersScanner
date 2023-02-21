51e7a73c72d396306a1c82d70f4c454926fc16db
Cootz <artem2004799@gmail.com>
2023-02-21 03:18:34 +0300

    Add `--port` option. Rename `IpAddresGenerator` to `NetworkHelper`

--- PiningMcServer/CLI/NetworkHelper.cs
+++ PiningMcServer/CLI/NetworkHelper.cs
@@ -3,9 +3,9 @@
 namespace McServersScanner.CLI
 {
     /// <summary>
-    /// Generates ranges of ip addresses depending on first and last addresses
+    /// Help with generating ranges of network stuff
     /// </summary>
-    public static class IpAddressGeneratior
+    public static class NetworkHelper
     {
         /// <summary>
         /// Generates ranges of ip addresses depending on first and last addresses
@@ -13,14 +13,12 @@ namespace McServersScanner.CLI
         /// <param name="first">Start of range</param>
         /// <param name="last">End of range</param>
         /// <returns>Range of ip addresses</returns>
-        public static IPAddress[] FillRange(IPAddress first, IPAddress last)
+        public static IPAddress[] FillIpRange(IPAddress first, IPAddress last)
         { 
             List<IPAddress> result = new List<IPAddress>();
 
             for (uint i = ConvertIpAddressToInt(first); i <= ConvertIpAddressToInt(last); i++)
-            {
                 result.Add(ConvertIntToIpAddress(i));
-            }
 
             return result.ToArray();
         }
@@ -31,7 +29,31 @@ namespace McServersScanner.CLI
         /// <param name="first">Start of range</param>
         /// <param name="last">End of range</param>
         /// <returns>Range of ip addresses</returns>
-        public static IPAddress[] FillRange(string first, string last) => FillRange(IPAddress.Parse(first), IPAddress.Parse(last));
+        public static IPAddress[] FillIpRange(string first, string last) => FillIpRange(IPAddress.Parse(first), IPAddress.Parse(last));
+
+        /// <summary>
+        /// Generates range of ports depending on first and last port
+        /// </summary>
+        /// <param name="first">Start of range</param>
+        /// <param name="last">End of range</param>
+        /// <returns>Range of ports</returns>
+        public static ushort[] FillPortRange(ushort first, ushort last)
+        {
+            List<ushort> result = new List<ushort>();
+
+            for (ushort i = first; i <= last; i++)
+                result.Add(i);
+
+            return result.ToArray();
+        }
+
+        /// <summary>
+        /// Generates range of ports depending on first and last port
+        /// </summary>
+        /// <param name="first">Start of range</param>
+        /// <param name="last">End of range</param>
+        /// <returns>Range of ports</returns>
+        public static ushort[] FillPortRange(string first, string last) => FillPortRange(ushort.Parse(first), ushort.Parse(last));
 
         /// <summary>
         /// Converts IpAddress to int
--- PiningMcServer/CLI/Options.cs
+++ PiningMcServer/CLI/Options.cs
@@ -7,7 +7,10 @@ namespace McServersScanner.CLI
     /// </summary>
     public class Options
     {
-        [Option('I', "range", Required = true, HelpText = "Ip adress range to be processed.")]
+        [Option('I', "range", Required = true, HelpText = "Ip address range to be processed.")]
         public IEnumerable<string>? Range { get; set; }
+
+        [Option('p', "port", Required = false, HelpText = "Range of ports to be scanned.")]
+        public IEnumerable<string>? Ports { get; set; }
     }
 }

209dd86f954950b798ce0e4fd355e494aa3321cd
Cootz <artem2004799@gmail.com>
2023-03-19 15:23:15 +0300

    Add test for `JsonHelper'

---
+++ McServersScanner.Tests/Utils/JsonHelperTest.cs
@@ -0,0 +1,19 @@
+﻿using McServersScanner.Utils;
+
+namespace McServersScanner.Tests.Utils
+{
+    [TestFixture]
+    public class JsonHelperTest
+    {
+        [Test]
+        public void ConvertingToJsonStringTest()
+        {
+            string dirtyJson = "{ \"TestData\" : \"Some test data\"}fjis:f;";
+            string expectedJson = "{ \"TestData\" : \"Some test data\"}";
+
+            string cleanJson = JsonHelper.ConvertToJsonString(dirtyJson);
+
+            Assert.That(cleanJson, Is.EqualTo(expectedJson));
+        }
+    }
+}

ac2fd05b9914708b23418f0950b53b1c810a323c
Cootz <artem2004799@gmail.com>
2023-03-30 23:39:04 +0300

    update release workflow

--- publish.cmd
+++ publish.cmd
@@ -1,3 +1,5 @@
+cd $1
+
 dotnet publish -r linux-x64 -c Release /p:PublishSingleFile=true
 dotnet publish -r win-x64 -c Release /p:PublishSingleFile=true
 dotnet publish -r osx-x64 -c Release /p:PublishSingleFile=true

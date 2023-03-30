b9f881b9fe5c52aeb49811fdb3f2f1387781c7d8
Cootz <artem2004799@gmail.com>
2023-03-30 23:45:23 +0300

    fix script typo

--- publish.cmd
+++ publish.cmd
@@ -10,8 +10,8 @@ mkdir publish-files
 cd linux-x64\publish
 tar -c -a -f ..\..\publish-files\linux-x64.zip *
 
-cd ..\..osx-x64\publish
+cd ..\..\osx-x64\publish
 tar -c -a -f ..\..\publish-files\osx-x64.zip *
 
-cd ..\..win-x64\publish
+cd ..\..\win-x64\publish
 tar -c -a -f ..\..\publish-files\win-x64.zip *

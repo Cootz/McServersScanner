e4270cf5ec21680bab8a4ecaf082329d6b5d277a
Cootz <artem2004799@gmail.com>
2023-04-12 11:47:41 +0300

    Add IOHelperTest

---
+++ McServersScanner.Tests/Utils/IOHelperTest.cs
@@ -0,0 +1,52 @@
+﻿using McServersScanner.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace McServersScanner.Tests.Utils
+{
+    [TestFixture]
+    public class IOHelperTest
+    {
+        const int N = 10;
+
+        [Test]
+        public void TestGetLinesCount()
+        {
+            string filePath = generateTestFile();
+
+            long linesCount = IOHelper.GetLinesCount(filePath);
+
+            Assert.That(linesCount, Is.EqualTo(N));
+        }
+
+        [Test]
+        public void TestReadFileLineByLine()
+        {
+            string filePath = generateTestFile();
+
+            IEnumerable<string> lines = IOHelper.ReadLineByLine(filePath);
+            IEnumerator<string> linesEnumerator = lines.GetEnumerator();
+
+            for (int i = 0; i < N; i++)
+            {
+                linesEnumerator.MoveNext();
+                Assert.That(linesEnumerator.Current, Is.EqualTo($"Line {i}"));
+            }
+        }
+
+        private string generateTestFile()
+        {
+            string filePath = Path.GetTempFileName();
+
+            using StreamWriter writer = new(filePath);
+
+            for (int i = 0; i < N; i++)
+                writer.WriteLine("Line {0}", i);
+
+            return filePath;
+        }
+    }
+}

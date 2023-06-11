4ce82c94fb7da97dd32e7bbe76ae1837433b9307
Cootz <artem2004799@gmail.com>
2023-06-11 17:17:45 +0300

    Move types into matching fields

--- McServersScanner.Core/Network/ThrottleManager.cs
+++ McServersScanner.Core/Network/ThrottleManager.cs
@@ -39,23 +39,4 @@ namespace McServersScanner.Core.Network
             return targetTime - actualTime;
         }
     }
-
-    internal static class WaitHandleHelper
-    {
-        public static Task WaitOneAsync(this WaitHandle waitHandle)
-        {
-            if (waitHandle == null)
-                throw new ArgumentNullException("waitHandle");
-
-            TaskCompletionSource<bool> tcs = new();
-
-            RegisteredWaitHandle rwh = ThreadPool.RegisterWaitForSingleObject(waitHandle,
-                delegate { tcs.TrySetResult(true); }, null, -1, true);
-
-            Task<bool> t = tcs.Task;
-            t.ContinueWith(_ => rwh.Unregister(null));
-
-            return t;
-        }
-    }
 }
---
+++ McServersScanner.Core/Network/WaitHandleHelper.cs
@@ -0,0 +1,20 @@
+﻿namespace McServersScanner.Core.Network;
+
+internal static class WaitHandleHelper
+{
+    public static Task WaitOneAsync(this WaitHandle waitHandle)
+    {
+        if (waitHandle == null)
+            throw new ArgumentNullException("waitHandle");
+
+        TaskCompletionSource<bool> tcs = new();
+
+        RegisteredWaitHandle rwh = ThreadPool.RegisterWaitForSingleObject(waitHandle,
+            delegate { tcs.TrySetResult(true); }, null, -1, true);
+
+        Task<bool> t = tcs.Task;
+        t.ContinueWith(_ => rwh.Unregister(null));
+
+        return t;
+    }
+}

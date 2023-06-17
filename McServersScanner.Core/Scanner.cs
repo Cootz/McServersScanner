c38c0ac3a7fbbc11f5ea96e9f2f2eecf965feb5b
Cootz <artem2004799@gmail.com>
2023-06-17 13:15:58 +0300

    Add todo for current progress *bar* implementation

--- McServersScanner.Core/Scanner.cs
+++ McServersScanner.Core/Scanner.cs
@@ -116,7 +116,7 @@ public sealed class Scanner : IScannerOptions
             //TODO: This shouldn't be bound to Console class. Change to Logger or any type of writable stream 
             Console.Write("\r{0:0.00}% - {1}/{2}", currentRatio, scannedIps, TotalIps);
 
-            await Task.Delay(100);
+            await Task.Delay(100); //TODO: this is pretty weird way to show progress
         }
 
         currentRatio = calculateRatio(scannedIps, TotalIps);

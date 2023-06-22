9ac8333a61aa41f6ad8e34cc65948b697196d11b
Cootz <artem2004799@gmail.com>
2023-06-22 13:17:27 +0300

    Add test for `McProtocol`

---
+++ McServersScanner.Tests/Network/McProtocolTests.cs
@@ -0,0 +1,20 @@
+﻿using McServersScanner.Core.Network;
+
+namespace McServersScanner.Tests.Network
+{
+    [TestFixture]
+    public class McProtocolTests
+    {
+        [Test]
+        public void WriteReadVarIntTest()
+        {
+            const int expectedValue = 25565;
+
+            byte[] valueBytes = McProtocol.WriteVarInt(expectedValue).ToArray();
+
+            int value = McProtocol.ReadVarInt(new MemoryStream(valueBytes));
+
+            Assert.That(value, Is.EqualTo(expectedValue));
+        }
+    }
+}

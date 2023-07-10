018dd5e05550b10414215c26bb5a5716fc49fe96
Cootz <artem2004799@gmail.com>
2023-07-10 08:27:06 +0300

    Add `ModInfo` model

---
+++ McServersScanner.Core/IO/Database/Models/ModInfo.cs
@@ -0,0 +1,21 @@
+﻿using Realms;
+
+namespace McServersScanner.Core.IO.Database.Models
+{
+    public class ModInfo : EmbeddedObject
+    {
+        public string ModId { get; private set; }
+
+        public string Version { get; private set; }
+
+        public ModInfo()
+        {
+        }
+
+        public ModInfo(string modId, string version)
+        {
+            ModId = modId;
+            Version = version;
+        }
+    }
+}

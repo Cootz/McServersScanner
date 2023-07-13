45889be3095e5e5a4f11a11efd40a750faef499a
Cootz <artem2004799@gmail.com>
2023-07-13 08:11:14 +0300

    Add test coverage for `ModInfo`

---
+++ McServersScanner.Tests/TestData/ModInfoDataSource.cs
@@ -0,0 +1,16 @@
+﻿using McServersScanner.Core.IO.Database.Models;
+
+namespace McServersScanner.Tests.TestData
+{
+    internal static class ModInfoDataSource
+    {
+        public static IEnumerable<TestCaseData> NegativeTestCases
+        {
+            get
+            {
+                yield return new TestCaseData(new ModInfo(), null);
+                yield return new TestCaseData(new ModInfo("TestID", "TestVersion"), new ModInfo());
+            }
+        }
+    }
+}
